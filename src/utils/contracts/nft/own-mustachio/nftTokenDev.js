4efa0754151975b2c5995bf70171ed3b5933cc79
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-03-10 16:51:13 +0800

    Update ABI for nftToken

--- src/utils/contracts/nft/own-mustachio/nftTokenDev.js
+++ src/utils/contracts/nft/own-mustachio/nftTokenDev.js
@@ -608,6 +608,49 @@ const nftTokenAbi = [
         "inputs":[
             {
                 "internalType":"uint256",
+                "name":"index",
+                "type":"uint256"
+            }
+        ],
+        "name":"tokenByIndex",
+        "outputs":[
+            {
+                "internalType":"uint256",
+                "name":"",
+                "type":"uint256"
+            }
+        ],
+        "stateMutability":"view",
+        "type":"function"
+    },
+    {
+        "inputs":[
+            {
+                "internalType":"address",
+                "name":"owner",
+                "type":"address"
+            },
+            {
+                "internalType":"uint256",
+                "name":"index",
+                "type":"uint256"
+            }
+        ],
+        "name":"tokenOfOwnerByIndex",
+        "outputs":[
+            {
+                "internalType":"uint256",
+                "name":"",
+                "type":"uint256"
+            }
+        ],
+        "stateMutability":"view",
+        "type":"function"
+    },
+    {
+        "inputs":[
+            {
+                "internalType":"uint256",
                 "name":"tokenId",
                 "type":"uint256"
             }
